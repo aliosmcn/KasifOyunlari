b9227d7b9cfdc37ac902cf54cda08ba89bcf982a
OZTRK14 <omerfarukozturk91@gmail.com>
2025-03-31 04:35:09 +0300

    Yeni Görseller

--- KasifOyunlari/Assets/Scenes/Oyun3/Kodlar/Zıpla.cs
+++ KasifOyunlari/Assets/Scenes/Oyun3/Kodlar/Zıpla.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Zıpla : MonoBehaviour
 {
     public float jumpcount;
     Rigidbody2D rb;
+    public Text skorT;
+    public float skor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        skor = 0;
     }
 
     // Update is called once per frame
@@ -17,6 +21,21 @@ public class Zıpla : MonoBehaviour
         {
            
             rb.linearVelocity = Vector2.up * jumpcount;
+        }
+        skorT.text = skor.ToString();
+    }
+    private void OnTriggerEnter2D(Collider2D temas)
+    {
+        if(temas.gameObject.tag=="Scorer")
+        {
+            skor++;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D temas)
+    {
+        if(temas.gameObject.tag =="Engel")
+        {
+            Time.timeScale = 0;
         }
     }
 }
